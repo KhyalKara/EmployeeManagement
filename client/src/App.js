4bdc70135d32fe383a7b97dad99306fd5df99523
ShashinGounden <101881243+ShashinGounden@users.noreply.github.com>
2023-09-25 00:56:26 +0200

    added basic insert operation
    Added basic insert operation

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,100 @@
+import React, { useState, useEffect } from 'react';
+import Axios from "axios";
 import './App.css';
 
 function App() {
+  const [employeeData, setEmployeeData] = useState({
+    employeeName: '',
+    employeeSurname: '',
+    employeeBirthDate: '',
+    employeeNumber: '',
+    employeeSalary: '',
+    employeeRole: '',
+    employeeReportingLineManager: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setEmployeeData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = () => {
+    Axios.post('http://localhost:3001/api/insert', {
+      ...employeeData // Spread the employeeData object to send all fields
+    })
+      .then(response => {
+        // Handle the response if needed
+        console.log(response);
+      })
+      .catch(error => {
+        // Handle errors if the request fails
+        console.error(error);
+      });
+  };
+
   return (
     <div>
       <div className="form">
         <label>Name:</label>
-        <input type="text" name="employeeName" />
+        <input
+          type="text"
+          name="employeeName"
+          value={employeeData.employeeName}
+          onChange={handleChange}
+        />
 
         <label>Surname:</label>
-        <input type="text" name="employeeSurname" />
+        <input
+          type="text"
+          name="employeeSurname"
+          value={employeeData.employeeSurname}
+          onChange={handleChange}
+        />
 
         <label>Birth Date:</label>
-        <input type="text" name="employeeBirthDate" />
+        <input
+          type="text"
+          name="employeeBirthDate"
+          value={employeeData.employeeBirthDate}
+          onChange={handleChange}
+        />
 
         <label>Employee Number:</label>
-        <input type="text" name="employeeNumber" />
+        <input
+          type="text"
+          name="employeeNumber"
+          value={employeeData.employeeNumber}
+          onChange={handleChange}
+        />
 
         <label>Salary:</label>
-        <input type="text" name="employeeSalary" />
+        <input
+          type="text"
+          name="employeeSalary"
+          value={employeeData.employeeSalary}
+          onChange={handleChange}
+        />
 
         <label>Role:</label>
-        <input type="text" name="employeeRole" />
+        <input
+          type="text"
+          name="employeeRole"
+          value={employeeData.employeeRole}
+          onChange={handleChange}
+        />
 
         <label>Reporting Line Manager:</label>
-        <input type="text" name="employeeReportingLineManager" />
+        <input
+          type="text"
+          name="employeeReportingLineManager"
+          value={employeeData.employeeReportingLineManager}
+          onChange={handleChange}
+        />
 
-        <button>Submit</button>
+        <button onClick={handleSubmit}>Submit</button>
       </div>
     </div>
   );
--- server/index.js
+++ server/index.js
@@ -1,7 +1,45 @@
 const express = require('express')
 const app = express()
+const mysql = require('mysql')
+const bodyParser = require('body-parser')
+const cors = require('cors')
 
-app.listen(3001, ()=>
-{
+const db = mysql.createPool(
+    {
+        host: 'localhost',
+      
+    }
+)
+
+app.use(cors());
+
+app.use(express.json())
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.get("/", (req, res) => {
+
+    /*  const sqlInsert = "INSERT INTO Employee_Data (Name, Surname, BirthDate, EmployeeNumber, Salary, Role, ReportingLineManager) VALUES ('a', 'a','a','a','a', 'a','a');"
+      db.query(sqlInsert, (err, result) => {
+          res.send(result);
+      }) */
+})
+
+app.post("/api/insert", (req, res) => {
+    const sqlInsert = "INSERT INTO Employee_Data (Name, Surname, BirthDate, EmployeeNumber, Salary, Role, ReportingLineManager) VALUES (?,?,?,?,?,?,?);"
+    const employeeName = req.body.employeeName;
+    const employeeSurname = req.body.employeeSurname;
+    const BirthDate = req.body.employeeBirthDate;
+    const employeeNumber = req.body.employeeNumber;
+    const employeeSalary = req.body.employeeSalary;
+    const employeeRole = req.body.employeeRole;
+    const reportingLineManager = req.body.employeeReportingLineManager;
+
+    db.query(sqlInsert, [employeeName, employeeSurname, BirthDate, employeeNumber, employeeSalary, employeeRole, reportingLineManager], (err, result) => {
+        console.log(err);
+    })
+})
+
+app.listen(3001, () => {
     console.log('running on port 3001');
 })
