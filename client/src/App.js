a00af20d0738270ad9a0c550894be87880419cb3
ShashinGounden <101881243+ShashinGounden@users.noreply.github.com>
2023-10-15 19:57:30 +0200

    Added manager hieararchy
    Added manager hieararchy

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import Axios from "axios";
 import './App.css';
+import md5 from 'md5';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus, faClose } from '@fortawesome/free-solid-svg-icons';
+
 
 function App() {
 
@@ -18,7 +22,8 @@ function App() {
     employeeNumber: '',
     employeeSalary: '',
     employeeRole: '',
-    employeeReportingLineManager: '',
+    employeeEmail: '',
+    employeeManager: null,
   });
 
   const [loadAllEmployeeData, setLoadAllEmployeeData] = useState([]);
@@ -28,7 +33,7 @@ function App() {
   const [editedEmployee, setEditedEmployee] = useState({}); // Track the edited employee data
 
 
-
+  const [errorMessage, setError] = useState(null);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -44,11 +49,53 @@ function App() {
     })
       .then(response => {
         // Handle the response if needed
-        console.log(response);
+        // console.log(response);
 
-        ;
+        // Check for server-side errors and display them
+        if (response.data.error) {
+          // Display the error message to the user
+          // You can use state to manage and display the error message in your UI
+          setError(response.data.error);
+
+        } else {
+          // Clear any previous error messages
+          setError(null);
+          // Close the modal or reset form, etc., on successful submission
+          setIsModalOpen(false);
+        }
       })
       .catch(error => {
+        // Handle errors if the request fails
+        console.error(error.response.data.error);
+        setError(error.response.data.error);
+        alert(error.response.data.error);
+      });
+  };
+
+
+  const handleSave = (index) => {
+    console.log(editedEmployee.EmployeeNumber);
+    Axios.put('http://localhost:3001/api/update/', {
+      employeeName: editedEmployee.first_name,
+      employeeSurname: editedEmployee.last_name,
+      employeeBirthDate: editedEmployee.birth_date,
+      employeeNumber: editedEmployee.employee_number,
+      employeeSalary: editedEmployee.salary,
+      employeeRole: editedEmployee.role,
+      employeeEmail: editedEmployee.email,
+    })
+      .then((response) => {
+        // Handle the response if needed
+        console.log(response);
+        setIsEditing(false); // Disable editing mode
+        // Update the local state with the edited employee data
+        setLoadAllEmployeeData((prevData) => {
+          const updatedData = [...prevData];
+          updatedData[index] = editedEmployee;
+          return updatedData;
+        });
+      })
+      .catch((error) => {
         // Handle errors if the request fails
         console.error(error);
       });
@@ -74,48 +121,63 @@ function App() {
     setIsEditing(false);
   };
 
-  const handleSave = (index) => {
-    console.log(editedEmployee.EmployeeNumber);
-    Axios.put('http://localhost:3001/api/update/', {
-      employeeName: editedEmployee.Name,
-      employeeSurname: editedEmployee.Surname,
-      employeeBirthDate: editedEmployee.BirthDate,
-      employeeNumber: editedEmployee.EmployeeNumber,
-      employeeSalary: editedEmployee.Salary,
-      employeeRole: editedEmployee.Role,
-      employeeReportingLineManager: editedEmployee.ReportingLineManager,
-    })
-      .then((response) => {
-        // Handle the response if needed
-        console.log(response);
-        setIsEditing(false); // Disable editing mode
-        // Update the local state with the edited employee data
-        setLoadAllEmployeeData((prevData) => {
-          const updatedData = [...prevData];
-          updatedData[index] = editedEmployee;
-          return updatedData;
-        });
-      })
-      .catch((error) => {
-        // Handle errors if the request fails
-        console.error(error);
-      });
+  function formatDate(dateString) {
+    const date = new Date(dateString);
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0'); // Adding 1 because months are 0-indexed
+    const day = String(date.getDate()).padStart(2, '0');
+    return `${year}-${month}-${day}`;
+  }
+
+  // Create a ref for the file input element
+  const fileInputRef = useRef(null);
+
+  // Function to trigger the file input click event
+  const handleImageUploadClick = () => {
+    if (fileInputRef.current) {
+      fileInputRef.current.click();
+    }
   };
 
-
-
+  // Function to handle image upload
+  const handleImageUpload = (event) => {
+    const selectedFile = event.target.files[0];
+    // Use Axios or any method to upload the selected image
+    // You can send it to your server and update the employee's image
+    if (selectedFile) {
+      // Handle the image upload here
+      // You can use Axios to send the file to the server
+      // Update the employee's image with the response
+    }
+  };
 
   return (
     <div className="container">
-      {/* Plus icon to toggle the modal */}
-      <button onClick={() => setIsModalOpen(true)} className="add-button">
-        +
-      </button>
 
       {isModalOpen && (
         <div className="modal">
-          <div className="form">
 
+          <div className="form">
+            {errorMessage && <div className="error-message">{errorMessage}</div>}
+            <h1 style={{ fontWeight: "400" }}>Enter employee details</h1>
+            <FontAwesomeIcon
+              icon={faClose}
+              className="close-icon"
+              onClick={() => setIsModalOpen(false)}
+              style={{
+                border: "none", width: "25", height: "25px", color: "grey", position: "absolute", right: "33%", top: "20%"
+              }}
+            />
+
+            <label>Email:</label>
+            <input
+              type="email"
+              name="employeeEmail"
+              value={employeeData.employeeEmail}
+              onChange={handleChange}
+              className='employeeInput'
+              placeholder='example@email.com'
+            />
 
             <label>Name:</label>
             <input
@@ -123,6 +185,8 @@ function App() {
               name="employeeName"
               value={employeeData.employeeName}
               onChange={handleChange}
+              className='employeeInput'
+              placeholder='John'
             />
 
             <label>Surname:</label>
@@ -131,14 +195,17 @@ function App() {
               name="employeeSurname"
               value={employeeData.employeeSurname}
               onChange={handleChange}
+              className='employeeInput'
+              placeholder='Doe'
             />
 
             <label>Birth Date:</label>
             <input
-              type="text"
+              type="date"
               name="employeeBirthDate"
               value={employeeData.employeeBirthDate}
               onChange={handleChange}
+              className='employeeInput'
             />
 
             <label>Employee Number:</label>
@@ -147,6 +214,8 @@ function App() {
               name="employeeNumber"
               value={employeeData.employeeNumber}
               onChange={handleChange}
+              className='employeeInput'
+              placeholder='123456'
             />
 
             <label>Salary:</label>
@@ -155,6 +224,8 @@ function App() {
               name="employeeSalary"
               value={employeeData.employeeSalary}
               onChange={handleChange}
+              className='employeeInput'
+              placeholder='200000'
             />
 
             <label>Role:</label>
@@ -163,135 +234,185 @@ function App() {
               name="employeeRole"
               value={employeeData.employeeRole}
               onChange={handleChange}
+              className='employeeInput'
+              placeholder='Technical Consultant'
             />
 
-            <label>Reporting Line Manager:</label>
+            <label>Reporting Manager:</label>
             <input
               type="text"
-              name="employeeReportingLineManager"
-              value={employeeData.employeeReportingLineManager}
+              name="employeeManager"
+              value={employeeData.employeeManager}
               onChange={handleChange}
+              className='employeeInput'
+              placeholder='e.g. 75648'
             />
 
-            <button onClick={handleSubmit}>Submit</button>
-            <button onClick={() => setIsModalOpen(false)} className="close-button">
-              Close
-            </button>
+            <button className='submitButton' onClick={handleSubmit}>Submit</button>
+
           </div>
         </div>
       )}
 
       <div className="employee-table">
+
+        <div className='main_heading_container'>
+          <h1 className='main_heading'>Employee Management System</h1>
+        </div>
+
         <table>
-          <thead>
+          <thead className='table_head'>
             <tr>
-              <th>Name</th>
-              <th>Surname</th>
-              <th>Birth Date</th>
-              <th>Employee Number</th>
-              <th>Salary</th>
-              <th>Role</th>
-              <th>Reporting Line Manager</th>
-              <th>Edit</th>
-              <th>Delete</th>
+              <th className="center">Employee Picture</th>
+              <th className="center">Name</th>
+              <th className="center">Surname</th>
+              <th className="center">Birth Date</th>
+              <th className="center">Employee Number</th>
+              <th className="center">Salary</th>
+              <th className="center">Role</th>
+              <th className="center">Email</th>
+              <th className="center">Reporting Manager</th>
+              <th className="center">Edit</th>
+              <th className="center">Delete</th>
             </tr>
           </thead>
           <tbody>
             {loadAllEmployeeData.map((employee, index) => (
               <tr key={index}>
-                <td>
+                <td className="center">
+                  <div className="profile-image-container">
+                    <img
+                      src={`https://www.gravatar.com/avatar/${md5(employee.email)}?d=identicon`}
+                      alt="Gravatar"
+                      className='profile_image'
+                    />
+                    <FontAwesomeIcon
+                      icon={faPlus}
+                      className="plus-icon"
+                      onClick={handleImageUploadClick}
+                      style={{
+                        border: "none", borderRadius: "50%", width: "15px", height: "15px", color: "white", backgroundColor: "#0056b3", position: "relative", left: "-15%"
+                      }}
+                    />
+                    {/* Hidden file input for image upload */}
+                    < input
+                      type="file"
+                      ref={fileInputRef}
+                      style={{ display: 'none' }}
+                      onChange={handleImageUpload}
+                    />
+                  </div>
+                </td>
+                <td className="center">
                   {isEditing === index ? (
                     <input
                       type="text"
                       name="employeeName"
-                      value={editedEmployee.Name}
-                      onChange={(e) => setEditedEmployee({ ...editedEmployee, Name: e.target.value })}
+                      value={editedEmployee.first_name}
+                      onChange={(e) => setEditedEmployee({ ...editedEmployee, first_name: e.target.value })}
                     />
                   ) : (
-                    employee.Name
+                    employee.first_name
                   )}
                 </td>
-                <td>
+                <td className="center">
                   {isEditing === index ? (
                     <input
                       type="text"
                       name="employeeSurname"
-                      value={editedEmployee.Surname}
-                      onChange={(e) => setEditedEmployee({ ...editedEmployee, Surname: e.target.value })}
+                      value={editedEmployee.last_name}
+                      onChange={(e) => setEditedEmployee({ ...editedEmployee, last_name: e.target.value })}
                     />
                   ) : (
-                    employee.Surname
+                    employee.last_name
                   )}
                 </td>
-                <td>
+                <td className="center">
                   {isEditing === index ? (
+
                     <input
                       type="text"
                       name="employeeBirthDate"
-                      value={editedEmployee.BirthDate}
-                      onChange={(e) => setEditedEmployee({ ...editedEmployee, BirthDate: e.target.value })}
+                      value={formatDate(editedEmployee.birth_date)}
+                      onChange={(e) => setEditedEmployee({ ...editedEmployee, birth_date: e.target.value })}
                     />
                   ) : (
-                    employee.BirthDate
+                    formatDate(employee.birth_date)
                   )}
                 </td>
-                <td>{employee.EmployeeNumber}</td>
-                <td>
+                <td className="center">
+                  {employee.employee_number}
+                </td>
+
+                <td className="center">
                   {isEditing === index ? (
                     <input
                       type="text"
                       name="employeeSalary"
-                      value={editedEmployee.Salary}
-                      onChange={(e) => setEditedEmployee({ ...editedEmployee, Salary: e.target.value })}
+                      value={editedEmployee.salary}
+                      onChange={(e) => setEditedEmployee({ ...editedEmployee, salary: e.target.value })}
                     />
                   ) : (
-                    employee.Salary
+                    employee.salary
                   )}
                 </td>
-                <td>
+
+                <td className="center">
                   {isEditing === index ? (
                     <input
                       type="text"
                       name="employeeRole"
-                      value={editedEmployee.Role}
-                      onChange={(e) => setEditedEmployee({ ...editedEmployee, Role: e.target.value })}
+                      value={editedEmployee.role}
+                      onChange={(e) => setEditedEmployee({ ...editedEmployee, role: e.target.value })}
                     />
                   ) : (
-                    employee.Role
+                    employee.role
                   )}
                 </td>
-                <td>
+
+                <td className="center">
                   {isEditing === index ? (
                     <input
                       type="text"
-                      name="employeeReportingLineManager"
-                      value={editedEmployee.ReportingLineManager}
-                      onChange={(e) =>
-                        setEditedEmployee({ ...editedEmployee, ReportingLineManager: e.target.value })
-                      }
+                      name="employeeEmail"
+                      value={editedEmployee.email}
+                      onChange={(e) => setEditedEmployee({ ...editedEmployee, email: e.target.value })}
                     />
                   ) : (
-                    employee.ReportingLineManager
+                    employee.email
                   )}
                 </td>
-                <td>
+
+                <td className="center">
+                  {employee.manager_first_name && employee.manager_last_name ? `${employee.manager_first_name} ${employee.manager_last_name}` : "None"}
+                  <br />
+                  {employee.manager_id ? "(" + employee.manager_id + ")" : ""}
+
+                </td>
+
+
+                <td className="center">
                   {isEditing === index ? (
                     <button onClick={() => handleSave(index)}>Save</button>
                   ) : (
                     <button onClick={() => handleEdit(employee, index)}>Edit</button>
                   )}
                 </td>
-                <td>
+                <td className="center">
                   {isEditing === index ? (
                     <button onClick={handleCancel}>Cancel</button>
                   ) : (
-                    <button onClick={() => handleDelete(employee.EmployeeNumber)}>Delete</button>
+                    <button onClick={() => handleDelete(employee.employee_number)}>Delete</button>
                   )}
                 </td>
               </tr>
             ))}
           </tbody>
         </table>
+        {/* Plus icon to toggle the modal */}
+        <button onClick={() => setIsModalOpen(true)} className="add-button">
+          +
+        </button>
       </div>
     </div>
 
--- server/index.js
+++ server/index.js
@@ -4,12 +4,19 @@ const mysql = require('mysql')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 
+
+require('dotenv').config()
+
 const db = mysql.createPool(
     {
         host: 'localhost',
-
+        user: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
     }
 )
+
+
 
 app.use(cors());
 
@@ -19,40 +26,58 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get("/", (req, res) => {
 
-    /*  const sqlInsert = "INSERT INTO Employee_Data (Name, Surname, BirthDate, EmployeeNumber, Salary, Role, ReportingLineManager) VALUES ('a', 'a','a','a','a', 'a','a');"
+    /*  const sqlInsert = "INSERT INTO Employee (Name, Surname, BirthDate, EmployeeNumber, Salary, Role) VALUES ('a', 'a','a','a','a', 'a','a');"
       db.query(sqlInsert, (err, result) => {
           res.send(result);
       }) */
 })
 
 app.post("/api/insert", (req, res) => {
-    const sqlInsert = "INSERT INTO Employee_Data (Name, Surname, BirthDate, EmployeeNumber, Salary, Role, ReportingLineManager) VALUES (?,?,?,?,?,?,?);"
-    const employeeName = req.body.employeeName;
-    const employeeSurname = req.body.employeeSurname;
-    const BirthDate = req.body.employeeBirthDate;
-    const employeeNumber = req.body.employeeNumber;
-    const employeeSalary = req.body.employeeSalary;
-    const employeeRole = req.body.employeeRole;
-    const reportingLineManager = req.body.employeeReportingLineManager;
+    const {
+        employeeName,
+        employeeSurname,
+        employeeBirthDate,
+        employeeNumber,
+        employeeSalary,
+        employeeRole,
+        employeeEmail,
+        employeeManager
+    } = req.body;
 
-    db.query(sqlInsert, [employeeName, employeeSurname, BirthDate, employeeNumber, employeeSalary, employeeRole, reportingLineManager], (err, result) => {
-        console.log(result);
-    })
-})
+    const formattedBirthDate = new Date(employeeBirthDate).toISOString().split('T')[0];
+    console.log(formattedBirthDate);
+
+    const sqlInsert = "INSERT INTO Employee (first_name, last_name, birth_date, employee_number, salary, role, email, manager_id) VALUES (?,?,?,?,?,?,?, ?);";
+
+    db.query(sqlInsert, [employeeName, employeeSurname, employeeBirthDate, employeeNumber, employeeSalary, employeeRole, employeeEmail, employeeManager],
+        (err, result) => {
+            if (err) {
+                console.log(err);
+            } else {
+                res.send(result);
+            }
+        })
+    // Rest of your code to insert the employee into the database
+});
+
 
 app.get("/api/get", (req, res) => {
-    const sqlSelect = "SELECT * FROM Employee_Data;"
-
+    const sqlSelect = `
+    SELECT e.*, m.first_name AS manager_first_name, m.last_name AS manager_last_name
+    FROM Employee e
+    LEFT JOIN Employee m ON e.manager_id = m.employee_number;
+`;
 
     db.query(sqlSelect, (err, result) => {
         res.send(result)
+        console.log(result);
     })
 })
 
 app.delete("/api/delete/:employeeNumber", (req, res) => {
     const employeeID = req.params.employeeNumber;
 
-    const sqlDelete = "DELETE FROM Employee_Data WHERE EmployeeNumber = ?";
+    const sqlDelete = "DELETE FROM Employee WHERE employee_number = ?";
 
     db.query(sqlDelete, employeeID, (err, result) => {
         if (err) {
@@ -70,19 +95,11 @@ app.put("/api/update", (req, res) => {
         employeeNumber,
         employeeSalary,
         employeeRole,
-        employeeReportingLineManager,
+        employeeEmail,
     } = req.body;
 
     const sqlUpdate = `
-          UPDATE Employee_Data
-          SET
-              Name = ?,
-              Surname = ?,
-              BirthDate = ?,
-              Salary = ?,
-              Role = ?,
-              ReportingLineManager = ?
-          WHERE EmployeeNumber = ?;
+          UPDATE Employee SET first_name = ?, last_name = ?, birth_date = ?, salary = ?, role = ?, email= ? WHERE employee_number = ?;
       `;
 
     db.query(
@@ -93,8 +110,8 @@ app.put("/api/update", (req, res) => {
             employeeBirthDate,
             employeeSalary,
             employeeRole,
-            employeeReportingLineManager,
-            employeeNumber,
+            employeeEmail,
+            employeeNumber
         ],
         (err, result) => {
             if (err) {
